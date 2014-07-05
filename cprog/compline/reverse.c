165a269e2614bdffc7d8562b9dd85608e2f85178
PalakPC <no.1palak@gmail.com>
2014-07-05 22:05:25 +0530

    Improvements

--- cprog/compline/reverse.c
+++ cprog/compline/reverse.c
@@ -1,24 +1,32 @@
+//C function to reverse the sequence of words in a string.
+
 #include <stdio.h>
 
-void reverse(char *str, int len) {
+void reverse (char *str, int len) {
 	char reversed[len+1];
 	int pos = 0;
 	int caret = len;
+	int i = len - 1; 	
 	reversed[len] = '\0';
-	int i = len - 1; 
-	while(i >= -1){
-		if (i == -1 || str[i] == ' '){
+
+	while (i >= -1) {
+		if (i == -1 || str[i] == ' ') {
 			int j = i + 1; 
-			while(j < caret){
+	
+			while (j < caret) {
 				reversed[pos++] = str[j];
 				j++;
 			}
+			
 			if (i != -1) {
 				reversed[pos++] = ' ';
 			}
+			
 			caret = i;
 		}
+		
 		i--;
 	}
-	printf("%s\n", reversed);
+	
+	printf ("%s\n", reversed);
 }
