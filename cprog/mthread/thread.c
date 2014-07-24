284183baa3021b3f7bf72fbcbd11da1603bea7ce
PalakPC <no.1palak@gmail.com>
2014-07-24 20:50:13 +0530

    mthread working, but not properly

--- cprog/mthread/thread.c
+++ cprog/mthread/thread.c
@@ -5,54 +5,52 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-pthread_mutex_t a_mutex = PTHREAD_MUTEX_INITIALIZER;
-
 void *doloop (void *data)
 {
 	int rc;
-	rc = pthread_mutex_lock(&a_mutex);
 	FILE *ofile;
 	ofile = fopen("test.txt", "a");
 	char a = 65 + (*((int *)data));
 	fputc(a, ofile);
 	close(ofile);
-	rc = pthread_mutex_unlock(&a_mutex);
+     pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[])
 {
-	srand (time(NULL));
 	int i, a, b, c = 0;
+	srand (time(NULL));
 	char s = ' ';
 	pthread_t pthread1[26];
 	int thr_id[26];
-	while(c < 500)
-	{
-		a = rand()%6 + 4;
+//	while(c < 500)
+//	{
+//		a = rand()%6 + 4;
 		/*printf ("%d", a);*/
 		b = 0; i=0;
 		for (i=0; i<26; ++i)
 		{
-			b++;
+//			b++;
 			thr_id[i] = pthread_create (&pthread1[i], NULL, doloop, (void *)&i);
-			if (b == a)
-			{
-				FILE *ofile2;
-				ofile2 = fopen("test.txt", "a");
-				fputc(s, ofile2);
-				b=0;
-				c++;
-				close(ofile2);
+	//		if (b == a)
+	//		{
+	//			FILE *ofile2;
+	//			ofile2 = fopen("test.txt", "a");
+	//			fputc(s, ofile2);
+	//			b=0;
+	//			c++;
+	//			close(ofile2);
 				/*b=0;*/
-				break;
-			}
+	//			break;
+	//		}
+
 		}
 		/*c++;
 		FILE *ofile2;
 		ofile2 = fopen("test.txt", "a");
 		fputc(s, ofile2);
 		close(ofile2);*/
-	}
+//	}
 	/*printf("%d %d", c, b);*/
 	return 0;
 }
