40a251bafe54ac785ebfe82f602b854b387d0581
PalakPC <no.1palak@gmail.com>
2014-07-31 16:51:58 +0530

    Multithreading code improved

--- cprog/mthread/mthread.c
+++ cprog/mthread/mthread.c
@@ -1,10 +1,13 @@
+/*C program to print alphabets to a file using separate threads for each alphabet.*/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define NUM_THREADS 26
 
-int a = 0, b = 0;
+int a = 0, b = 1, len = 0;    /*Had to make b = 1 as otherwise it prints an extra word. I don't see why.*/
 
 void *PrintHello (void *threadid) {
      char tid, s = ' ';
@@ -14,12 +17,14 @@ void *PrintHello (void *threadid) {
      ofile = fopen ("test.txt", "a");
      fputc (tid, ofile);
      a++;
-     if (a == 2) {
+     if (a >= len) {
         fputc (s, ofile);
         a = 0;
+        len = rand () % 6 + 4;
+        printf ("%d\n", len);
         b++;
      }
-     if (b > 5) {
+     if (b >= 50) {
         exit (0);
      }
      close (ofile);
@@ -27,14 +32,21 @@ void *PrintHello (void *threadid) {
 }
 
 int main (int argc, char *argv[]) {
+     srand (time (NULL));
+     len = rand () % 6 + 4;
+     printf ("%d\n", len);
      pthread_t threads[NUM_THREADS];
      int rc, t;
-     for (t = 0; t < NUM_THREADS ; t++) {
+     void * status;
+l1:  for (t = 0; t < NUM_THREADS ; t++) {
         rc = pthread_create (&threads[t], NULL, PrintHello, (void *) t);
         if (rc) {
            printf ("ERROR; return code from pthread_create() is %d\n", rc);
            exit (-1);
         }
+        pthread_join (threads[t], &status);
      }
+     if (b < 50)
+        goto l1;
      pthread_exit(NULL);
 }
